e6f2396cdaee7d938334ed9480d3e8714d1a9aca
Daniel <71837281+darmiel@users.noreply.github.com>
2022-07-12 10:17:35 +0200

    fix: chat being weird

--- pkg/handlers/chat.go
+++ pkg/handlers/chat.go
@@ -3,33 +3,20 @@ package handlers
 import (
 	"github.com/Qwiri/gobby/pkg/gobby"
 	"github.com/Qwiri/gobby/pkg/validate"
-	"github.com/apex/log"
 )
 
 //goland:noinspection GoUnusedGlobalVariable
 var Chat = &gobby.Handler{
 	Validation: validate.Schemes{
-		validate.String().Min(1).Max(16).As("user"),
-		validate.String().Min(1).Max(256).As("message"),
+		validate.String().Min(1).Max(512).As("message"),
 	},
 	Handler: func(event *gobby.Handle) error {
-		user := event.String("user")
 		message := event.String("message")
 
 		// build socket message and send to every client in lobby
-		msg := gobby.NewBasicMessage("CHAT", user, message)
+		msg := gobby.NewBasicMessage("CHAT", event.Client.Name, message)
+		event.Lobby.BroadcastForce(msg)
 
-		var err error
-		for _, c := range event.Lobby.Clients {
-			if c == event.Client {
-				err = event.Message.ReplyWith(c, *msg)
-			} else {
-				err = msg.SendTo(c)
-			}
-			if err != nil {
-				log.WithError(err).Warnf("cannot send chat message to %s", c.Name)
-			}
-		}
 		return nil
 	},
 }
